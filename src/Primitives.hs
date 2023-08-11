afa02c8a58b8e7bcf0be4c7e88b55374a78d1d51
shundroid <shundroid@users.noreply.github.com>
2023-08-11 09:55:57 +0900

    added ++ fn

--- src/Primitives.hs
+++ src/Primitives.hs
@@ -9,18 +9,22 @@ data Primitive = Primitive {
   pOuts :: [String],
   pEval :: FactoryValue }
 
-arithFoldL2FactoryValue :: (Int -> Int -> Int) -> Int -> FactoryValue
-arithFoldL2FactoryValue f z _ inParticles maxIn outParticles _ = do
+foldL2FactoryValue :: (Value -> Value -> Value) -> Value -> FactoryValue
+foldL2FactoryValue f z _ inParticles maxIn outParticles _ = do
   if not (null outParticles) || length inParticles < maxIn
   then return $ Just (InNoOp, OutNoOp, Nothing)
-  else return $ Just (InFlush, OutAppend [("result", VNum (foldl (\a (_, VNum x) -> f a x) z inParticles))], Nothing)
-arith2Ope2FactoryValue :: (Int -> Int -> Int) -> FactoryValue
-arith2Ope2FactoryValue f _ inParticles maxIn outParticles _ = do
-  if not (null outParticles) || length inParticles < maxIn
+  else return $ Just (InFlush, OutAppend [("result", foldl (\a (_, x) -> f a x) z inParticles)], Nothing)
+arithFoldL2FactoryValue :: (Int -> Int -> Int) -> Int -> FactoryValue
+arithFoldL2FactoryValue f z = foldL2FactoryValue (\(VNum x) (VNum y) -> VNum (f x y)) (VNum z)
+twoOpe2FactoryValue :: (Value -> Value -> Value) -> FactoryValue
+twoOpe2FactoryValue f _ inParticles _ outParticles _ = do
+  if not (null outParticles) || length inParticles < 2
   then return $ Just (InNoOp, OutNoOp, Nothing)
   else do
-    let [(_, VNum x), (_, VNum y)] = inParticles
-    return $ Just (InFlush, OutAppend [("result", VNum (f x y))], Nothing)
+    let [(_, x), (_, y)] = inParticles
+    return $ Just (InFlush, OutAppend [("result", f x y)], Nothing)
+arith2Ope2FactoryValue :: (Int -> Int -> Int) -> FactoryValue
+arith2Ope2FactoryValue f = twoOpe2FactoryValue (\(VNum x) (VNum y) -> VNum (f x y))
 
 doOutput v = do
   putStr $ show v
@@ -37,6 +41,7 @@ primitives = Map.fromList [
     ("*", Primitive ["a", "b"] ["result"] $ arithFoldL2FactoryValue (*) 1),
     ("/", Primitive ["a", "b"] ["result"] $ arith2Ope2FactoryValue div),
     ("==", Primitive ["a", "b"] ["result"] $ arith2Ope2FactoryValue (\a b -> if a == b then 1 else 0)),
+    ("++", Primitive ["a", "b"] ["result"] $ foldL2FactoryValue (\(VString x) (VString y) -> VString (x ++ y)) $ VString ""),
     ("output", Primitive ["a"] [] $ \_ [(_, v)] _ _ _ -> do
       doOutput v
       return $ Just (InFlush, OutNoOp, Nothing)),
