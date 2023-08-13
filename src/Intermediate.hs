43f76179d14a238f838784a683e85c44c1779828
shundroid <shundroid@users.noreply.github.com>
2023-08-13 18:12:09 +0900

    minor fix

--- src/Intermediate.hs
+++ src/Intermediate.hs
@@ -239,6 +239,6 @@ handleError (PrimitiveNotFoundError info name ins) s =
 handleError (MultiDrivenRefError info name) s =
   "The ref " ++ name ++ " is multi-driven. " ++ infoToString info s
 handleError (ArgsMatchingError info args ins) s =
-  "The arguments of " ++ infoToString info s ++ " is not appropriate. Appropriate args: " ++ show args ++ "\ninternal: " ++ show ins
+  "The arguments of " ++ infoToString info s ++ " are not appropriate. Appropriate args: " ++ show args ++ "\ninternal: " ++ show ins
 handleError (InternalError msg infos) s =
   "Sorry, an internal error occurred. " ++ msg ++ "\ndump: " ++ show (map (\i -> infoToString i s) infos)
